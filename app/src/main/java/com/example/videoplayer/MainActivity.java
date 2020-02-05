7629907b392b565c8cf88f3eaa90f9a1646d8307
xiedong11 <xiedong11@aliyun.com>
2020-02-05 20:59:53 +0800

    搭建项目页面框架

--- app/src/main/java/com/example/videoplayer/MainActivity.java
+++ app/src/main/java/com/example/videoplayer/MainActivity.java
@@ -1,14 +1,84 @@
 package com.example.videoplayer;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.MenuItem;
 
-import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.ViewPager;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.videoplayer.adapter.HomePageAdapter;
+import com.example.videoplayer.business.tab.HomeFragment;
+import com.example.videoplayer.business.tab.LocalVideoFragment;
+import com.example.videoplayer.business.tab.MineFragment;
+import com.example.videoplayer.business.tab.OnLineVideoFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.zhuandian.base.BaseActivity;
+import com.zhuandian.base.BaseFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+
+public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.vp_home)
+    ViewPager vpHome;
+    @BindView(R.id.tab_bottom)
+    BottomNavigationView tabBottom;
+    private static final int PAGE_HOME = 0;
+    private static final int PAGE_LOCAL_VIDEO = 1;
+    private static final int PAGE_ONLINE_VIDEO = 2;
+    private static final int PAGE_MY = 3;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void setUpView() {
+        List<BaseFragment> fragmentList = new ArrayList<>();
+        fragmentList.add(new HomeFragment());
+        fragmentList.add(new LocalVideoFragment());
+        fragmentList.add(new OnLineVideoFragment());
+        fragmentList.add(new MineFragment());
+        vpHome.setAdapter(new HomePageAdapter(getSupportFragmentManager(), fragmentList));
+        vpHome.setOffscreenPageLimit(4);
+
+        vpHome.setCurrentItem(PAGE_HOME);
+        initBottomTab();
+    }
+
+    private void initBottomTab() {
+        vpHome.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener(){
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabBottom.getMenu().getItem(position).setChecked(true);
+            }
+        });
+
+        tabBottom.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+               switch (item.getItemId()){
+                   case R.id.tab_home:
+                       vpHome.setCurrentItem(PAGE_HOME);
+                       break;
+                   case R.id.tab_local:
+                       vpHome.setCurrentItem(PAGE_LOCAL_VIDEO);
+                       break;
+                   case R.id.tab_online:
+                       vpHome.setCurrentItem(PAGE_ONLINE_VIDEO);
+                       break;
+                   case R.id.tab_my:
+                       vpHome.setCurrentItem(PAGE_MY);
+                       break;
+               }
+
+                return true;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/videoplayer/business/tab/OnLineVideoFragment.java
@@ -0,0 +1,21 @@
+package com.example.videoplayer.business.tab;
+
+import com.example.videoplayer.R;
+import com.zhuandian.base.BaseFragment;
+
+/**
+ * desc :
+ * author：xiedong
+ * date：2020/02/05
+ */
+public class OnLineVideoFragment extends BaseFragment {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_online_video;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
